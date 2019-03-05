c44536ce5db91c39eefbed0b465d16b485c2547e
Vinyas <vinyas@doselect.com>
2019-03-05 09:17:18 +0530

    Fix for exec error

--- django_extensions/management/commands/shell_plus.py
+++ django_extensions/management/commands/shell_plus.py
@@ -291,8 +291,9 @@ class Command(BaseCommand):
                     pythonrc_code = handle.read()
                 # Match the behavior of the cpython shell where an error in
                 # PYTHONSTARTUP prints an exception and continues.
+                # https://github.com/django-extensions/django-extensions/commit/50a0e4ccd88be5186bc3282307e7e51169865d79
                 try:
-                    exec(compile(pythonrc_code, pythonrc, 'exec'), imported_objects)
+                    exec(compile(pythonrc_code, pythonrc, 'exec'), imported_objects) in globals(), locals()
                 except Exception:
                     traceback.print_exc()
                     if self.tests_mode:
